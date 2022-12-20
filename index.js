500e8c07784d0fcf4d7bb67ac74a01832a47e3a0
jopsfernandes <joaopfernandessantos@gmail.com>
2022-12-20 16:19:57 -0400

    criando e alterando arquivos de texto com node

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const http = require('http');
 const fs = require('fs')
+
+
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
+//Cria um novo arquivo 
+fs.writeFile('ex.txt', 'texto exemplo', (err) =>{
+    if(err) throw err; 
+    console.log('o arquivo foi criado com sucesso!')
+})
+
+//Cria um novo arquivo ou insere mais conteúdo caso já exista um arquivo com tal nome
+fs.appendFile('ex.txt', '\noutro conteúdo', (err) =>{
+    if(err) throw err;
+    console.log('arquivo alterado com sucesso')
+})
+
 
 const server = http.createServer((req,res) => {
 
