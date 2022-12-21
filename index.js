b394a5accb00f7d2cffc74b6b8877cce8584b7f4
jopsfernandes <joaopfernandessantos@gmail.com>
2022-12-20 20:33:48 -0400

    manipulação de arquivos com o método readFile do fs

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const read = require('readline');
 
+fs.readFile('ex.txt',(err,data) => {
 
-const hostname = '127.0.0.1';
-const port = 3000;
+    let str = data.toString();
+    // let newStr = str.split('/')
 
-
-//Cria um novo arquivo 
-fs.writeFile('ex.txt', 'texto exemplo', (err) =>{
-    if(err) throw err; 
-    console.log('o arquivo foi criado com sucesso!')
+    let newStr = str.substring(0,3);
+    console.log(newStr)
 })
-
-//Cria um novo arquivo ou insere mais conteúdo caso já exista um arquivo com tal nome
-fs.appendFile('ex.txt', '\noutro conteúdo', (err) =>{
-    if(err) throw err;
-    console.log('arquivo alterado com sucesso')
-})
-
-
-const server = http.createServer((req,res) => {
-
-    if (req.url == '/jops'){
-    fs.readFile('index.html', (err, data ) =>{
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
-
-    }else{return res.end();}
-})
-
-server.listen(port, hostname,() =>{ console.log('servidor rodando')})
